d6800cc2235828269b1fac0fdd6e35b37f2f68c1
Ксения Осипова <xeni26061994@mail.ru>
2023-11-11 18:22:37 +0300

    Final version

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello, World!')
+import re
+
+
+def is_valid_hex_color(color):
+    pattern = re.compile(r"^#[a-fA-F0-9]{6}$")
+
+    return bool(pattern.match(color))
+
+
+hex_color = input('Введите цвет в формате HEX: ')
+
+if is_valid_hex_color(hex_color):
+    print(f"{hex_color} - это корректный цвет в формате HEX.")
+else:
+    print(f"{hex_color} - не является корректным цветом в формате HEX.")
